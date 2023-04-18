ef472c76deb98efc4e77b89f9dbd957c99dbb206
ltg <ltg@bbangyanetworks.com>
2023-04-19 06:12:52 +0900

    [Section6] 마무리

--- src/disk/disk.service.ts
+++ src/disk/disk.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from '../power/power.service';
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+  constructor(private powerservice: PowerService) {}
+
+  getData() {
+    console.log('Drawing 20 watts of power from PowerService');
+    this.powerservice.supplyPower(20);
+    return 'data!';
+  }
+}
